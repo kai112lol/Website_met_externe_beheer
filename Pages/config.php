823e2d56f43c09e436de40bb8a5c66cceca50382
kai112lol <kai@hiraki.nl>
2026-04-22 15:08:15 +0200

    comments toegevoegd

--- Pages/config.php
+++ Pages/config.php
@@ -19,7 +19,8 @@ try {
     die("Databaseverbinding mislukt: " . $e->getMessage());
 }
 
-
+// Ik heb hier AS gebruikt omdat ik hier twee names heb eentje voor de genre en de ander voor het platform
+//ik heb hier join gebruik omdat ik van meerdere databases dingen moet halen en op eentje mergen
 $sql = "SELECT g.game_id, g.title, g.description, g.released_at, g.personal_rating, ge.name AS genre_name, p.name AS platform_name, g.rawg_id, g.rawg_rating, g.created_at, g.updated_at FROM games g LEFT JOIN genres ge ON g.genre_id = ge.genre_id LEFT JOIN platforms p ON g.platform_id = p.platform_id ORDER BY g.game_id DESC";
 
 $gameresult = $conn->query($sql);
